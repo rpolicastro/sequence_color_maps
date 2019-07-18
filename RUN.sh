c4fc65da44fa1334f35fc2bbcf55bd7b6fbfa278
rpolicastro <rpolicas@indiana.edu>
2019-07-18 17:42:14 -0400

    integrating singularity

--- RUN.sh
+++ RUN.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+CONTAINER="library://rpolicastro/default/sequence_colormaps:1.0.0"
 
+###############################
+## Generate Sequence Colormaps
+###############################
+
+## Prepare Container
+## ----------
+
+CONTAINER_NAME=$(basename $CONTAINER | tr ":" "_")".sif"
+
+if [ ! -f "singularity/${CONTAINER_NAME}" ]; then
+	singularity pull $CONTAINER
+	mv $CONTAINER_NAME singularity
+fi
+
+## Run Script
+## ----------
+
+singularity exec -eC \
+"singularity/${CONTAINER_NAME}" \
+main.R
