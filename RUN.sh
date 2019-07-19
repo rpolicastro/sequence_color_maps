5c9d69014b6601f5de66bc8aa34fd5e9e4e0a98f
rpolicastro <rpolicas@indiana.edu>
2019-07-18 21:01:18 -0400

    allowing singularity container to access fasta

--- RUN.sh
+++ RUN.sh
@@ -19,7 +19,14 @@ fi
 ## Run Script
 ## ----------
 
+## Get directory that has the fasta file.
+
+FASTA=$(sed -n 's/^fasta\.file.*\"\(.*\)\"$/\1/p' settings.conf)
+FASTA_DIR=$(dirname FASTA)
+
+## Run script using software in container.
+
 singularity exec \
--e -C -B $PWD -H $PWD \
+-e -C -B ${PWD},${FASTA_DIR} -H $PWD \
 "singularity/${CONTAINER_NAME}" \
 Rscript main.R
