96cc95aa5721abe231c4bb70841295e962766e7d
rpolicastro <rpolicas@indiana.edu>
2019-07-21 18:44:45 -0400

    also have it export sequence logo

--- main.R
+++ main.R
@@ -2,6 +2,7 @@
 
 library("Biostrings")
 library("tidyverse")
+library("ggseqlogo")
 
 ################################
 ## Plotting Sequence Color Maps
@@ -25,7 +26,12 @@ seq.length <- FASTA %>%
 	map_dbl(~nchar(.)) %>%
 	unique
 
-## Prepare data for plotting.
+if (length(seq.length) > 1) {stop("Not all sequences are the same length")}
+
+## Plot Heatmap
+## ----------
+
+## Prepare data.
 
 cleaned.data <- FASTA %>%
 	as.character %>%
@@ -37,8 +43,7 @@ cleaned.data <- FASTA %>%
 	gather(key="Position", value="base", -sequence) %>%
 	mutate(Position=as.integer(Position))
 
-## Plotting Data
-## ----------
+## Plot data.
 
 p <- ggplot(cleaned.data, aes(x=Position, y=sequence)) +
 	geom_tile(aes(fill=base)) +
@@ -53,3 +58,29 @@ p <- ggplot(cleaned.data, aes(x=Position, y=sequence)) +
 	)
 
 ggsave("sequence-colormap.pdf", plot=p, device="pdf", width=5, height=5)
+
+## Make Sequence Logo
+## ----------
+
+## Prepare position frequency matrix.
+
+PFM <- consensusMatrix(FASTA, as.prob=TRUE) %>%
+	.[rownames(.) %in% c("A", "C", "G", "T"),]
+
+## Set viridis base color for plotting.
+
+viridis.bases <- make_col_scheme(
+	chars=c("A", "C", "G", "T"),
+	groups=c("A", "C", "G", "T"),
+	cols=c("#431352", "#34698c", "#44b57b", "#fde540")
+)
+
+## Plot sequence logo.
+
+p <- ggplot() +
+	geom_logo(PFM, col_scheme=viridis.bases) +
+	theme_logo()
+
+ggsave("sequence-logo.pdf", plot=p, device="png", width=5, height=1.5)
+
+
